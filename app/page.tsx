38931df33b52652be5f914d17bd21e6695994e59
rydeebs <rnd5061@gmail.com>
2026-05-31 09:41:46 -0400

    Use provided popup and app logos

--- app/page.tsx
+++ app/page.tsx
@@ -170,7 +170,7 @@ export default function Home() {
           <img
             alt="Moment Guessr"
             className="game-brand-logo"
-            src="/moment-popup/logo-full.png"
+            src="/moment-popup/logo-app-white.png"
           />
         </div>
         <div className="game-timer pointer-events-auto absolute left-1/2 top-3 -translate-x-1/2 sm:top-6">
